d16ee19c9bdd17d7827039bbe84eecca49363fbe
Todor <to6esko69@gmail.com>
2019-01-29 07:12:29 +0200

    3 commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
+import axios from 'axios';
+
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit=this.handleSubmit.bind(this);
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+ async handleSubmit(e) {
+    e.preventDefault();
+
+    const { name, email, message } = this.state;
+    const from = await axios.post('/api/form',{
+      name,
+      email,
+      message
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Form onSubmit={this.handleSubmit} style={{ width: '600px', marginLeft: '50px', marginTop: '50px' }}>
+        <FormGroup>
+          <Label for="name">Name</Label>
+          <Input
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="name">Email</Label>
+          <Input
+            type="text"
+            name="name"
+            onChange={this.handleChange} />
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="name">Message</Label>
+          <Input
+            type="text"
+            name="name"
+            onChange={this.handleChange} />
+        </FormGroup>
+
+        <Button>Submit</Button>
+      </Form>
+
     );
   }
 }
-
 export default App;
